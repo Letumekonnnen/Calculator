045dad45dbc20354d8e7490a86f7e852a1247b02
Letu Mekonnen <letinamekonnen6@gmail.com>
2024-01-10 12:11:23 +0300

    added

--- calc.py
+++ calc.py
@@ -9,3 +9,11 @@ def subtract(x, y):
 
 def multiply(x, y):
     return x * y
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
+def power(x, y):
+    return x ** y
